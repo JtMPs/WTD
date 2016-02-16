c4e27d89b24d880ff630648b301e936a5152621b
Rootowsky <lazy2394@gmail.com>
2016-02-16 02:56:08 +0100

    field correct

--- field.h
+++ field.h
@@ -5,10 +5,10 @@
 class Field {
 public:
     enum Object {platform};
-    using object = std::vector<Object, std::pair<int, int>>;
+    using object = std::pair<Object, std::pair<int, int>>;
     enum Edge {left, right, up, down};  
     Field(std::vector<object> objects) : objects(objects) {}
-    Field(std::vector<object> objects, std::pair<Field, Edge> neigbours[4])
+    Field(std::vector<object> objects, std::pair<Field*, Edge> neigbours[4])
         : objects(objects) {for(int i = 0; i < 4; i++) this->neighbours[i] = neigbours[i];}
     void set_neighbour(Edge e, Field * f, Edge ef) { neighbours[e] = std::make_pair(f, ef); }
     std::pair<Field*, Edge> get_edge(Edge e) {return neighbours[e];}
