4f629e4a134951f16947150eedb37dd12cf943a0
JtMPs <jacek.mj.kubica@gmail.com>
2016-02-16 01:18:09 +0100

    Field class

---
+++ field.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <utility>
+#include <vector>
+
+class Field {
+public:
+    enum Object {platform};
+    using object = std::vector<Object, std::pair<int, int>>;
+    enum Edge {left, right, up, down};  
+    Field(std::vector<object> objects) : objects(objects) {}
+    Field(std::vector<object> objects, std::pair<Field, Edge> neigbours[4])
+        : objects(objects) {for(int i = 0; i < 4; i++) this->neighbours[i] = neigbours[i];}
+    void set_neighbour(Edge e, Field * f, Edge ef) { neighbours[e] = std::make_pair(f, ef); }
+    std::pair<Field*, Edge> get_edge(Edge e) {return neighbours[e];}
+private:
+    std::pair<Field*, Edge> neighbours[4];  
+    std::vector<object> objects;
+};
